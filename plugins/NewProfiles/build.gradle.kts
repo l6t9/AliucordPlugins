fb9a350b5561a933872becbbef35fef062e9c414
LampDelivery <158360149+l6t9@users.noreply.github.com>
2026-03-30 19:46:22 -0400

    removed old junk

--- plugins/NewProfiles/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-version = "0.0.2"
-description = "Allows you to view new user profiles"
-
-aliucord.changelog.set("""
-    # 0.0.2
-    * voice settings, admin actions, edit profiles sections are now transparent too
-    * user details sheet's window inset area is now also colorized
-""".trimIndent())
