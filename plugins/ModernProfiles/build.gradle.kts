c8f26d76301404c3640ffd19d41a248f63eccd51
LampDelivery <158360149+l6t9@users.noreply.github.com>
2026-04-09 01:04:32 -0400

    light mode profiles

--- plugins/ModernProfiles/build.gradle.kts
+++ plugins/ModernProfiles/build.gradle.kts
@@ -1,6 +1,12 @@
-version = "1.0.1"
+version = "1.0.2"
 description = "Allows you to view new user profiles, but modernized"
 
 aliucord {
     deploy.set(true)
 }
+
+aliucord.changelog.set("""
+    # 1.0.2
+    * Light mode profiles are now supported, which means you can now flashbang yourself using Aliucord! You're welcome. 
+    * Oh and dark mode profiles stay the same. Yeah.
+""".trimIndent())
